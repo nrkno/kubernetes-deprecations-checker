0d50608e2b10cc9d20882c3b96824547ee688733
Mikkel Nygard <mikkel.nygard@nrk.no>
2023-11-21 16:25:31 +0100

    feat: update output formatting

--- deprecations.sh
+++ deprecations.sh
@@ -2,7 +2,6 @@
 objects=$(cat deprecations.json)
 keys=($(echo $objects | jq 'keys[]'))
 for key in "${keys[@]}"; do
-    # echo -e "\v\t\a\nChecking for deprecations from kubernetes version: $(echo $key | xargs)"
     resources=$(echo $objects | jq -r .$key)
     resource_keys=($(echo $resources | jq 'keys[]'))
 
@@ -11,10 +10,10 @@ for key in "${keys[@]}"; do
         res=$(echo $res | xargs)
         echo -e "\v\t\a$(echo $key | xargs): Checking for $res of $api_version"
         kubectl get $(echo $res | xargs) -A -o json | jq -r --arg api_version "$api_version" --arg res "$res" '
-            ["Name", "Namespace", "Resource", "Api Version"],
-            (.items[] | select(.apiVersion == $api_version) | [.metadata.name, .metadata.namespace, $res, $api_version])
+            ["Resource", "Api Version", "Name", "Namespace"],
+            (.items[] | select(.apiVersion == $api_version) | [$res, $api_version, .metadata.name, .metadata.namespace ])
             | @tsv
-            ' | column -t -s $'\t' -c 2
+            ' | column -t -s $'\t'
 
     done
 done
