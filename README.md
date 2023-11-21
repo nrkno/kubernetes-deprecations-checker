fb2468c2dae387aa52455e6cdf00698fc43dacd0
Mikkel Nygard <mikkel.nygard@nrk.no>
2023-11-21 15:23:27 +0100

    chore: rename objects.json to deprecations.json

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Simple shell script to check for deprecations in your kubernetes cluster
 Starting at kubernetes version 1.25.
-Manual updates to `objects.json` is needed when new kubernetes deprecations are announced.
+Manual updates to `deprecations.json` is needed when new kubernetes deprecations are announced.
 
 ## Dependencies:
 ```
--- deprecations.sh
+++ deprecations.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
-objects=$(cat objects.json)
+objects=$(cat deprecations.json)
 keys=($(echo $objects | jq 'keys[]'))
 for key in "${keys[@]}"; do
-    echo -e "\v\t\a\nChecking for deprecations from kubernetes version: $(echo $key | xargs)"
+    # echo -e "\v\t\a\nChecking for deprecations from kubernetes version: $(echo $key | xargs)"
     resources=$(echo $objects | jq -r .$key)
     resource_keys=($(echo $resources | jq 'keys[]'))
 
     for res in "${resource_keys[@]}"; do
         api_version=$(echo $resources | jq -r .$res)
-        echo -e "\v\t\a$(echo $key | xargs): Checking for $(echo $res | xargs) of $api_version"
-        kubectl get $(echo $res | xargs) -A -o json | jq -r '.items[] | select(.apiVersion == "'"$api_version"'") | "\(.metadata.name)\t\(.metadata.namespace)"' | column -t -s $'\t'
+        res=$(echo $res | xargs)
+        echo -e "\v\t\a$(echo $key | xargs): Checking for $res of $api_version"
+        kubectl get $(echo $res | xargs) -A -o json | jq -r --arg api_version "$api_version" --arg res "$res" '
+            ["Name", "Namespace", "Resource", "Api Version"],
+            (.items[] | select(.apiVersion == $api_version) | [.metadata.name, .metadata.namespace, $res, $api_version])
+            | @tsv
+            ' | column -t -s $'\t' -c 2
+
+
     done
 done
